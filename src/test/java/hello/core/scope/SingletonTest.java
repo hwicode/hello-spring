8c0eb96942b3fcc6716c71567847ca756b5cdec4
hwicode <khc3796@naver.com>
2022-09-18 23:57:29 +0900

    SingletonClass -> SingletonTest로 이름 변경

--- src/test/java/hello/core/scope/SingletonTest.java
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-public class SingletonClass {
+public class SingletonTest {
 
     @Test
     void singletonBeanFind() {
